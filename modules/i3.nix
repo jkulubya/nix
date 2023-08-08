2098b961d5f3f9b43ad00a0465e0be74e78f3451
JK <James>
2023-08-08 14:06:08 +0200

    Tweak i3 font sizes

--- modules/i3.nix
+++ modules/i3.nix
@@ -17,11 +17,15 @@
         modifier = "Mod4";
         fonts = {
           names = ["DejaVu Sans Mono, FontAwesome 6 Free"];
+          size = 11.0;
         };    
         bars = [
           {
             position = "bottom";
             statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
+            fonts = {
+              size = 13.0;
+            };
           }
         ];
         keybindings = lib.mkOptionDefault {
@@ -52,12 +56,41 @@
     enable = true;
     bars = {
       bottom = {
+        icons = "awesome6";
+        settings = {
+          theme = {
+            theme = "slick";
+            overrides = {
+              separator = "";
+            };
+          };
+        };
         blocks = [
-         {
-           block = "time";
-           interval = 60;
-           format = "%a %d/%m %k:%M %p";
-         }
+          {
+            block = "cpu";
+          }
+          {
+            alert = 10.0;
+            block = "disk_space";
+            format = " $icon root: $available.eng(w:2) ";
+            info_type = "available";
+            interval = 20;
+            path = "/";
+            warning = 20.0;
+          }
+          {
+            block = "memory";
+            format = " $icon $mem_total_used_percents.eng(w:2) ";
+            format_alt = " $icon_swap $swap_used_percents.eng(w:2) ";
+          }
+          {
+            block = "sound";
+          }
+          {
+            block = "time";
+            format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+            interval = 5;
+          }
        ];
       };
     };
